79285f929fe5b072b563a102240895c8ab18d887
Cristiano <cristiano.tarconi@gmail.com>
2021-01-25 23:35:28 +0100

    testing click method on vue

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ let app = new Vue({
     el: '#container',
     data: {
         dim: 'dimension',
-        message: 'Prova di testo aggiunto utilizzando Vue!',
+        message: 'Clicca sull\'immagine per ingrandirla',
         src: 'https://images.unsplash.com/photo-1611561937862-9d0527d25f63?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
         title: 'beach'
-    }
-    /* methods: {
-        clickImmagine() {
-            this.message = 'Ciao!'
+    },
+    methods: {
+        clickImage() {
+            (this.dim == 'dimension') ? this.dim = 'dimension_big' : this.dim = 'dimension';
         }
-    } */
+    }
 
 });
