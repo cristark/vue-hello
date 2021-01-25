177f8724e3240862e44dd763c3ed748836f719ed
Cristiano <cristiano.tarconi@gmail.com>
2021-01-25 17:35:57 +0100

    add img w/ src, data, class using vue

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ let app = new Vue({
 
     el: '#container',
     data: {
-        message: 'Prova di testo aggiunto utilizzando Vue!'
+        dim: 'dimension',
+        message: 'Prova di testo aggiunto utilizzando Vue!',
+        src: 'https://images.unsplash.com/photo-1611561937862-9d0527d25f63?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+        title: 'beach'
     }
+    /* methods: {
+        clickImmagine() {
+            this.message = 'Ciao!'
+        }
+    } */
 
 });
