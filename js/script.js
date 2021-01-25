94e6f117db28b60903a2e5d462a32406af71f86a
Cristiano <cristiano.tarconi@gmail.com>
2021-01-25 16:27:35 +0100

    print text using vue

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 /* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus: Aggiungere un’immagine presa anch’essa da un data assieme al valore alt dell'attributo. Poi aggiungete all'immagine una classe sempre presa dal data. */
 
+let app = new Vue({
+
+    el: '#container',
+    data: {
+        message: 'Prova di testo aggiunto utilizzando Vue!'
+    }
+
+});
